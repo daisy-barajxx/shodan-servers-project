48832790fabed250feec194167c0af83e0887d38
Daisy Barajas-Manzo <daisybarajas135@gmail.com>
2025-05-04 12:04:09 -0700

    thorough analysis

--- lint-deb.sh
+++ lint-deb.sh
@@ -3,5 +3,6 @@
 # lint the debian package using lintian
 echo "Linting the debian package..."
 # run lintian to check the deb package
-lintian shodansvc-v1.0.0.deb
+#lintian shodansvc-v1.0.0.deb
 
+lintian --verbose --no-tag-display-limit shodansvc-v1.0.0.deb
