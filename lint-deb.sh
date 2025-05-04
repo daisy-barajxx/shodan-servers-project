7c796766fba120274738f3e1a6ee700ed63b8860
Daisy Barajas-Manzo <daisybarajas135@gmail.com>
2025-05-04 12:07:54 -0700

    added a - to the front of the lintian command

--- lint-deb.sh
+++ lint-deb.sh
@@ -3,6 +3,5 @@
 # lint the debian package using lintian
 echo "Linting the debian package..."
 # run lintian to check the deb package
-#lintian shodansvc-v1.0.0.deb
+-lintian shodansvc-v1.0.0.deb
 
-lintian --verbose --no-tag-display-limit shodansvc-v1.0.0.deb
